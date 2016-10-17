f35575647cc4c6bb88ab4fde85474e39921a02ec
Tomasz Tomczyk <me@tomasztomczyk.com>
2016-10-17 21:12:33 +0100

    Space age

---
+++ elixir/space-age/space_age.exs
@@ -0,0 +1,22 @@
+defmodule SpaceAge do
+  @type planet :: :mercury | :venus | :earth | :mars | :jupiter
+                | :saturn | :neptune | :uranus
+
+  @earth_year 31557600
+  @orbital_period %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(:earth, seconds), do: seconds / @earth_year
+  def age_on(planet, seconds), do: age_on(:earth, seconds) / @orbital_period[planet]
+end
